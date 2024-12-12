36f545ddb3aa586d6bcfeac02bef64c61513dc51
Pia <piaborgoglio80@gmail.com>
2024-12-12 00:23:20 -0300

    primera parte homework M1L3 js

--- scripts/index.js
+++ scripts/index.js
@@ -36,3 +36,39 @@ class Repository {
         this.activities = this.activities.filter((actividad) => actividad.id !== id);
     }
 }
+
+
+function createActivityCard(activity) {
+    
+    const { id, title, description, imgURL } = activity;
+
+   
+    const cardDiv = document.createElement('div');
+    const titleElement = document.createElement('h3');
+    const descriptionElement = document.createElement('p');
+    const imgElement = document.createElement('img');
+    const closeButton = document.createElement('span');
+
+ 
+    titleElement.innerHTML = title;
+    descriptionElement.innerHTML = description;
+    imgElement.src = imgURL;
+    imgElement.alt = `Imagen de ${title}`;
+    closeButton.innerHTML = 'X';
+
+    
+    cardDiv.classList.add('activity-card');
+    titleElement.classList.add('activity-title');
+    descriptionElement.classList.add('activity-description');
+    imgElement.classList.add('activity-img');
+    closeButton.classList.add('x');
+
+    
+    cardDiv.appendChild(titleElement);
+    cardDiv.appendChild(closeButton);
+    cardDiv.appendChild(imgElement);
+    cardDiv.appendChild(descriptionElement);
+
+    
+    return cardDiv;
+}
